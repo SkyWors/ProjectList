e8b336afcdb8ac5dd1c4a31516a037203b5cba33
SkyWors <skyworslegrand@gmail.com>
2026-08-29 16:45:31 +0200

    ♻️ Account langs

--- src/views/layouts/login/index.php
+++ src/views/layouts/login/index.php
@@ -1,6 +1,5 @@
 <?php
 	use App\Enums\Path;
-	use Tempora\Utils\Lang;
 ?>
 
 <?php include Path::COMPONENT_NAVBARS->value . "/navbar.php"; ?>
--- src/views/layouts/register/index.php
+++ src/views/layouts/register/index.php
@@ -1,13 +1,12 @@
 <?php
 	use App\Enums\Path;
-	use Tempora\Utils\Lang;
 ?>
 
 <?php include Path::COMPONENT_NAVBARS->value . "/navbar.php"; ?>
 
 <main>
 	<div class="register_container">
-		<h1><?= Lang::translate(key: "REGISTER_TITLE") ?></h1>
+		<h1><?= $pageLang->translate(key: "REGISTER_TITLE") ?></h1>
 
 		<?php include Path::COMPONENT_FORMS->value . "/register_form.php"; ?>
 	</div>
